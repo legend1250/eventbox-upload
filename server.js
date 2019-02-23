24bc809b538c6cae5abaa1030e278c6b273e5d2e
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2019-02-23 15:10:46 +0700

    get ticket endpoint

--- server.js
+++ server.js
@@ -160,6 +160,34 @@ app.get('/image/:filename', (req, res) => {
   });
 });
 
+// @route GET /ticket/:filename
+// @desc Display ticket svg
+app.get('/ticket/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/svg+xml') {
+      // Read output to browser
+      res.writeHead(200, {
+        'Content-Type': 'image/svg+xml',
+      })
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res)
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+
 // @route DELETE /files/:id
 // @desc  Delete file
 app.delete('/files/:id', (req, res) => {
